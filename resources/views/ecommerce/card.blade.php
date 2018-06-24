5f20ee0dd27a5014c552f3162a685a8db6812ba9
Renan <renan_gian@hotmail.com>
2018-06-23 22:14:10 -0300

    add links card

--- resources/views/ecommerce/card.blade.php
+++ resources/views/ecommerce/card.blade.php
@@ -6,13 +6,21 @@
     <div class="card-body">
       <div class="d-flex justify-content-between">
       <h5 class="card-title"> {{ $p->nome }}</h5>
-       <p style="color:blue">  R$ {{ $p->preco }} </p>
+       <p style="color:blue">  R$ {{ number_format($p->preco, 2) }}  </p>
       </div>
       <hr>
 
       <p class="card-text">
          {{ $p->descricao }}
       </p>
+      <hr>
+      <a href="#" class="card-link">
+          Detalhes
+      </a>
+      <a href="#" class="card-link">
+        <i class="fas fa-cart-plus"></i>
+        Adicionar ao carrinho
+      </a>
     </div>
     
   </div>
