e5795a7674c599ddcf6c11ef4ba1cdaae20e243b
Renan <renan_gian@hotmail.com>
2018-06-24 13:32:56 -0300

    add estilo carrinho'

--- resources/views/ecommerce/carrinho/index.blade.php
+++ resources/views/ecommerce/carrinho/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+  @include('ecommerce.carrinho.items')
+  @include('ecommerce.carrinho.resultado')
 @endsection
 
---
+++ resources/views/ecommerce/carrinho/resultado.blade.php
@@ -0,0 +1,13 @@
+<div class="d-flex justify-content-end">
+  <div class="shadow-lg p-3 col-md-11 mb-5 bg-white rounded justify-content-between d-flex" style="height: 80px;">
+      <div class="btn">
+        <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+              Finalizar Compra
+        </button>
+      </div>
+
+      <div class="resultado align-self-center">
+          <p style="font-size:1.6rem;"> R$ 900 </p>
+      </div>
+</div>
+</div>
